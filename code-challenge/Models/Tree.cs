9aa5aeaf30b204e0632b91e0a00902cdf2b41bde
Samuel Tregea <sdtregea@gmail.com>
2021-11-08 10:22:53 -0500

    Updated comments for the generic tree model.

--- code-challenge/Models/Tree.cs
+++ code-challenge/Models/Tree.cs
@@ -1,11 +1,14 @@
 ﻿namespace challenge.Models
 {
     /// <summary>
-    /// 
+    /// A generic Tree structure.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">The Tree's type.</typeparam>
     public class Tree<T>
     {
+        /// <summary>
+        /// The root node for the tree.
+        /// </summary>
         public Node<T> Root { get; set; }
     }
 }
